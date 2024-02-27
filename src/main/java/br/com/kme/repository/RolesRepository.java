f6c8d22102cb06b049592423018a3303cc2f1e1f
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2024-02-27 11:45:55 -0300

    Criado repositório de roles.

---
+++ src/main/java/br/com/kme/repository/RolesRepository.java
@@ -0,0 +1,24 @@
+package br.com.kme.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.kme.entities.Role;
+
+@Repository
+public interface RolesRepository  extends JpaRepository<Role, Integer>{
+	
+	@Query("SELECT r "
+			+ "FROM Role r "
+			+ "WHERE r.id = :id")
+	public Role findBy(Integer id);
+	
+	@Modifying
+	@Query("DELETE "
+			+ "FROM Role r "
+			+ "WHERE r.id = :id")
+	public Role deleteBy(Integer id);
+
+}
