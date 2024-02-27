2c493da147dcc9dc5f6fbf410ec5eb8c9097ae92
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2024-02-27 11:45:03 -0300

    Criada entidade access_scope.

---
+++ src/main/java/br/com/kme/entities/AccessScope.java
@@ -0,0 +1,43 @@
+package br.com.kme.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity(name = "AccessScope")
+@Table(name = "access_scope")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class AccessScope {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+    private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "role_id")
+	@NotNull(message = "O papel é obrigatório. ")
+	private Role role;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "permission_id")
+	@NotNull(message = "A permissão é obrigatória. ")
+	private Permission permission;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	@NotNull(message = "O usuário é obrigatório.")
+	private User user;
+	
+}
